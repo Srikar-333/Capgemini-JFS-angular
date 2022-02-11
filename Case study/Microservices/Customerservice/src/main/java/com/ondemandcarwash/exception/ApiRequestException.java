d9d0aaa0fa414e795ae6e2793244ccf8ee08bbbd
Srikar-333 <69790635+Srikar-333@users.noreply.github.com>
2022-02-12 00:23:33 +0530

    Add files via upload

---
+++ Case study/Microservices/Customerservice/src/main/java/com/ondemandcarwash/exception/ApiRequestException.java
@@ -0,0 +1,9 @@
+package com.ondemandcarwash.exception;
+
+public class ApiRequestException extends Exception {
+
+	public ApiRequestException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
